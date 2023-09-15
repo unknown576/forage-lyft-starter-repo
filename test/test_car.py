7533c603f9e0e9a96edf1a3253bf58d6e0653fda
unknown576 <62588524+unknown576@users.noreply.github.com>
2023-09-15 02:39:41 +0200

    Update test_car.py

--- test/test_car.py
+++ test/test_car.py
@@ -12,7 +12,7 @@ class TestCalliope(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
         last_service_date = today.replace(year=today.year - 3)
-        current_mileage = 1
+        current_mileage = 0
         last_service_mileage = 0
 
         car = Calliope(last_service_date, current_mileage, last_service_mileage)
